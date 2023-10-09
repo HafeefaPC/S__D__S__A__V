b7e707477504efe60c1a7c80922d3ad7dcdf16d1
HafeefaPC <hafeefapc2003@gmail.com>
2023-10-09 22:18:34 +0530

    1 to n print

---
+++ recursion/1 to n.cpp
@@ -0,0 +1,28 @@
+void fun(vector<string> &ans , int n ){
+
+   if(n==0)  return;
+
+    ans.push_back("Coding Ninjas");
+ 
+
+    fun(ans , n-1);     
+
+}
+
+ 
+
+ 
+
+vector<string> printNTimes(int n) {    
+
+   vector<string> ans;
+
+ 
+
+    fun(ans , n);
+
+ 
+
+    return ans;
+
+ }
