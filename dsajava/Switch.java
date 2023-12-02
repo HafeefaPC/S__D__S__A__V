54d1eac7b3ff74d95d611484169a66e4ace9ba77
Hafeefa <hafeefapc2003@gmail.com>
2023-12-02 23:35:58 +0530

    switch

---
+++ dsajava/Switch.java
@@ -0,0 +1,26 @@
+package dsajava;
+import java.util.Scanner;
+public class Switch{
+    public static void main(String[] args){
+    Scanner in = new Scanner (System.in);
+    System.out.println("enter the fruit name");
+    String fruit = in.next();
+    switch(fruit){
+        case "mango":
+        System.out.println("king of fruits");
+        break;
+        case "apple":
+        System.out.println("a sweet red fruit");
+        break;
+        case "orange":
+        System.out.println("round fruit");
+        break;
+        case "grapes":
+        System.out.println("small fruit");
+        break;
+        default:
+        System.out.println("please enter a valid fruit");   
+        
+    }
+    }
+}
