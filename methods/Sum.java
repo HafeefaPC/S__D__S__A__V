adeeb44d14fb2168134922fe3d226e0b51108dea
Hafeefa <hafeefapc2003@gmail.com>
2023-12-04 20:46:36 +0530

    methods1

---
+++ methods/Sum.java
@@ -0,0 +1,20 @@
+package methods;
+
+
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        sum();
+    }
+    static void sum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number");
+        int a = sc.nextInt();
+        System.out.println("enter the second number");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("the sum is : " + sum);
+    }
+}
