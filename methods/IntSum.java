10b3ce508bb5bffb5482043076041acb19de5887
Hafeefa <hafeefapc2003@gmail.com>
2023-12-04 21:03:13 +0530

    methodInt

---
+++ methods/IntSum.java
@@ -0,0 +1,11 @@
+package methods;
+
+
+public class IntSum {
+    public static void main(String[] args){
+        System.out.println("The sum is : " + sum(5,6));
+    }
+    static int sum(int a,int b){
+       return a+b;
+    }
+}
