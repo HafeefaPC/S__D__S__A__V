a7cd04abe5e1bdde80b7e4559d20340edb275d03
Hafeefa <hafeefapc2003@gmail.com>
2023-12-08 21:07:25 +0530

    array1

---
+++ array/Basic.java
@@ -0,0 +1,15 @@
+package array;
+import java.util.Scanner;
+public class Basic{
+        public static void main(String[] args){
+            int[] hai = new int[20];
+            Scanner sc = new Scanner(System.in);
+            for( int i=0;i<5;i++){
+             hai[i]=sc.nextInt();
+            }
+            for( int i=0;i<5;i++){
+             System.out.print(hai[i] + " ");
+            }
+
+        }
+}
