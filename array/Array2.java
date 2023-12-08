75da2b3cd0eb1adc2ed5b783b6abe2e392c3356c
Hafeefa <hafeefapc2003@gmail.com>
2023-12-09 00:49:24 +0530

    array

---
+++ array/Array2.java
@@ -0,0 +1,14 @@
+package array;
+import java.util.Scanner;
+public class Array2 {
+    public static void main(String[] args){
+      System.out.println("enter the 2D array");
+      int[][] arr =new arr[3][];
+      for(int row=0;row<arr.length;row++){
+          for(int col=0;col<arr[row].length;coo++)
+          {
+            System.out.print(arr[row][col]);
+          }
+      }
+    }
+}
