d63cf036265b67b334e1b831f427e3267710606c
Hafeefa <hafeefapc2003@gmail.com>
2023-12-14 20:12:06 +0530

    evendigit other way

--- search/Evennumdigit.java
+++ search/Evennumdigit.java
@@ -1,33 +1,43 @@
 package search;
 
+
 public class Evennumdigit {
     public static void main(String[] args) {
     int[] nums = {18,24,1,5,7};
     System.out.println(even(nums));
     }
 
-    static int count(int num) {
+    // static int count(int num) {
 
-        int count = 0;
+    //     int count = 0;
         
-        while (num > 0 || num < 0) {
+    //     while (num > 0 || num < 0) {
              
-            count++;
-            num = num / 10;
-        }
+    //         count++;
+    //         num = num / 10;
+    //     }
 
            
             
-        return count;
-    }
+    //     return count;
+    // }
 
-    static int even(int[] arr) {
-        int even = 0;
-        for (int num : arr){
-        if (count(num) % 2 == 0) {
-            even++;
-        }   
+        static int even(int[] arr) {
+        //     int even = 0;
+        //     for (int num : arr){
+        //     if (count(num) % 2 == 0) {
+        //         even++;
+        //     }   
+        // }
+        // return even;
+         int even=0;
+        for(int num : arr){
+
+            int a = (int) Math.log10(Math.abs(num)) + 1;
+            if(a%2==0){
+               even++;
+            }
+        }
+        return even;
     }
-    return even;
-}
 }
