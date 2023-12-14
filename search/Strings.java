04f1eafeb0998408434ea7f09c479bc6a7eda3cc
Hafeefa <hafeefapc2003@gmail.com>
2023-12-14 07:24:08 +0530

    stringsearch

--- search/Strings.java
+++ search/Strings.java
@@ -1,5 +1,23 @@
 package search;
 
 public class Strings {
+  public static void main(String[] args){
+    String name = "hafefa";
+    char t ='a';
+    boolean ans = search(name , t);
+    System.out.println(ans);
+  }
 
+   static boolean search(String n , char u){
+    if(n.length() ==0){
+        return false;
+    }
+
+    for(int i =0;i< n.length(); i++){
+        if( u == n.charAt(i)){
+            return true;
+        }
+    }
+    return false;
+  }
 }
