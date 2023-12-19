b3ea46fcdc4832dc74966f86207424b0becf9928
Hafeefa <hafeefapc2003@gmail.com>
2023-12-19 09:46:05 +0530

    foor

--- binarysearch/ceiling.java
+++ binarysearch/ceiling.java
@@ -2,25 +2,26 @@
 package binarysearch;
 
 public class ceiling {
-    public static void main(String[] args) {
-        int[] arr = { 2, 4, 16, 20, 22, 31, 35 };
-        int target = 25;
-        System.out.println(celing(arr, target));
-    }
-
-    static int celing(int[] arr, int target) {
-        int s = 0;
-        int e = arr.length - 1;
-        while (s <= e) {
-            int mid = (s + e) / 2;
-            if (target < arr[mid]) {
-                e = mid - 1;
-            } else if (target > arr[mid]) {
-                s = mid + 1;
-            } else {
-                return mid;
-            }
+    public static void main(String[] args){
+    int[] arr ={2,4,16,20,22,31,35} ;
+   int target=25;
+   System.out.println(celing(arr, target));
+}
+static int celing(int[] arr,int target){
+    int s=0;
+    int e=arr.length-1;
+    while(s<=e){
+        int mid=(s+e)/2;
+        if(target<arr[mid]){
+            e=mid-1;
         }
-        return s;
+        else if(target>arr[mid]){
+            s=mid+1;
+        }
+        else{
+            return mid;
+        }
     }
+    return s ;
+}
 }
---
+++ binarysearch/floor.java
@@ -0,0 +1,25 @@
+package binarysearch;
+
+public class floor {
+    public static void main(String[] args){
+        int[] arr = { 2, 4, 16, 20, 22, 31, 35 };
+        int target = 25;
+        System.out.println(flor(arr, target)); 
+    }
+
+    static int flor(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (target < arr[mid]) {
+                e = mid - 1;
+            } else if (target > arr[mid]) {
+                s = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return e;
+    }
+}
