a4eff41960a9a106e806cb360c02a194faae2919
Hafeefa <hafeefapc2003@gmail.com>
2024-02-02 16:57:06 +0530

    rotating array

--- binarysearch/rotatingarray.java
+++ binarysearch/rotatingarray.java
@@ -4,8 +4,26 @@ package binarysearch;
 
 public class rotatingarray {
     public static void main(String[] args) {
-        int[] arr ={4,5,6,7,2,1,0};
+        int[] arr = { 4, 5, 6, 7, 2, 1, 0 };
         System.out.println(findpivot(arr));
+    }
+
+    static int search(int[] arr,int target){
+        int pivot = findpivot(arr);
+
+         //if pivot id not found just do normal binary search
+        if(pivot == -1){
+            return binary(arr, target,0,arr.length-1);
+        }
+        if(arr[pivot] == target){
+            return pivot;
+        }
+        if(target >= arr[0]){
+            return binary(arr, target,0,pivot-1);
+        }
+        
+            return binary(arr,target,pivot+1, arr.length-1);
+        
     }
 
     static int findpivot(int[] arr) {
@@ -28,4 +46,22 @@ public class rotatingarray {
         }
         return -1;
     }
+
+    static int binary(int[] arr ,int target,int start,int end ){
+
+        while (start < end) {
+            int mid = ((start + end) / 2) + 1;
+            if(target<arr[mid]){
+                end=mid-1;
+            }
+            else if(target>arr[mid]){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+        
+    } 
 }
